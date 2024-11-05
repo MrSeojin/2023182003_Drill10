1a5b03597ebec4efc8eff8bb7602aebdd5653830
Seojin.K <7701seojin@gmail.com>
2024-11-05 10:24:26 +0900

    title 생성

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import game_framework
 #import play_mod as start_mode
-import logo_mod as start_mode
+import title_mode as start_mode
 
 open_canvas()
 game_framework.run(start_mode)
