b0d91f64573012fa05328a6fe62a16f60512271f
Seojin.K <7701seojin@gmail.com>
2024-11-11 15:20:12 +0900

    Drill10 제출 (2023182003 김서진)

---
+++ bird.py
@@ -0,0 +1,55 @@
+# 이것은 각 상태들을 객체로 구현한 것임.
+
+from pico2d import get_time, load_image, SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT, load_font
+from state_machine import *
+from ball import Ball
+import game_world
+import random
+import game_framework
+
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+class Bird:
+    def __init__(self):
+        self.font = load_font('ENCR10B.TTF', 16)
+        self.frame, self.action = 0, 0
+        size = random.randint(1, 5)
+        self.w, self.h = 183 / size, 168 / size
+        self.x, self.y = 400, 400
+        self.speed = random.randint(1, 5)
+
+        self.rad, self.flip = 0, 0
+        self.dir = 1
+        self.image = load_image('bird_animation.png')
+
+    def update(self):
+        self.frame = self.frame + self.speed * FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time
+        if self.action < 2 and self.frame >= 4:
+            self.action += 1
+            self.frame = self.frame % 4
+        elif self.action == 2 and self.frame >= 3:
+            self.action = 0
+            self.frame = self.frame % 3
+        self.x += self.dir * self.speed * RUN_SPEED_PPS * game_framework.frame_time
+        if self.x <= self.w / 2:
+            self.dir *= -1
+            self.x = self.w / 2
+        elif self.x >= 1600 - self.w / 2:
+            self.dir *= -1
+            self.x = 1600 - self.w / 2
+
+    def draw(self):
+        if self.dir == 1:
+            self.image.clip_draw(int(self.frame) * 183, self.action * 168, 183, 168, self.x, self.y, self.w, self.h)
+        else:
+            self.image.clip_composite_draw(int(self.frame) * 183, self.action * 168, 183, 168, self.rad, 'h', self.x, self.y, self.w, self.h)
+        self.font.draw(self.x - 60, self.y + 50, f'(Time: {get_time():.2f})', (255, 255, 0))
--- play_mode.py
+++ play_mode.py
@@ -2,6 +2,7 @@ from pico2d import *
 import game_framework
 
 import game_world
+from bird import Bird
 from grass import Grass
 from boy import Boy
 
@@ -20,12 +21,20 @@ def handle_events():
 def init():
     global grass
     global boy
+    global birds
 
     grass = Grass()
     game_world.add_object(grass, 0)
 
     boy = Boy()
     game_world.add_object(boy, 1)
+
+#    birds = [Bird() for i in range(10)]
+#    for o in birds:
+#        game_world.add_object(o, 1)
+
+    birds = Bird()
+    game_world.add_object(birds, 1)
 
 
 def finish():
